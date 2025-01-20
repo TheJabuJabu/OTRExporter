247169afbb5f30f2f1eda394fdb095fe2605c0de
TheJabuJabu <TheJabuJabu@proton.me>
2025-01-20 14:32:50 -0700

    Added documentation

--- OTRExporter/TextExporter.cpp
+++ OTRExporter/TextExporter.cpp
@@ -1,6 +1,33 @@
+/**
+ * @file TextExporter.cpp
+ * @brief Implements text resource export functionality for OTR format
+ *
+ * Handles the export of ZText resources into the binary OTR format, including:
+ * - Message collections
+ * - Text properties and formatting
+ * - Control codes and special characters
+ */
+
 #include "TextExporter.h"
 #include "../ZAPD/ZFile.h"
+#include <fstream>
 
+/**
+ * @brief Exports a text resource to OTR binary format
+ * 
+ * Serializes a ZText resource into the standardized OTR binary format with:
+ * 1. Standard OTR resource header with type and size
+ * 2. Message count as uint32
+ * 3. For each message:
+ *    - Message ID (uint16) for lookup
+ *    - Textbox type (uint8) for visual style
+ *    - Textbox Y position (uint8) for screen placement
+ *    - Full message string including control codes
+ *
+ * @param res The text resource to export (must be ZText*)
+ * @param outPath Path where the OTR file will be written
+ * @param writer Binary writer for serializing the data
+ */
 void OTRExporter_Text::Save(ZResource* res, const fs::path& outPath, BinaryWriter* writer)
 {
 	ZText* txt = (ZText*)res;
